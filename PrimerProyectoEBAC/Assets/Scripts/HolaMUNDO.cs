e50b2e5c2db435bc3157799d5dccba1db37d1697
Eduardo Zoe Samperio Cabrera <drakoeldragon1@gmail.com>
2022-09-13 20:07:15 -0500

    Hola Mundo
    Se agregaron scripts de debugeo a main camara

---
+++ PrimerProyectoEBAC/Assets/Scripts/HolaMUNDO.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMUNDO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("ALGO PASÓ");
+        Debug.Log("Algo psaó");
+        Debug.LogWarning("Algo salio medio mal");
+        Debug.LogError("Algfo salio muy mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
